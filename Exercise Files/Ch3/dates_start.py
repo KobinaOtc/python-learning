1961a247a953ace2673e892b363e4f764eac0996
kotchere <kotchere@verisage.us>
2019-01-21 19:19:28 +0000

    excerises complete

--- Exercise Files/Ch3/dates_start.py
+++ Exercise Files/Ch3/dates_start.py
@@ -1,25 +1,33 @@
 #
 # Example file for working with date information
 #
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 
 def main():
-  ## DATE OBJECTS
-  # Get today's date from the simple today() method from the date class
+  # ## DATE OBJECTS
+  # # Get today's date from the simple today() method from the date class
+  # today = date.today()
+  # print("Today's date is ", today)
 
-
-  # print out the date's individual components
-
+  # # print out the date's individual components
+  # print("Date Components: ", today.day, today.month, today.year)
   
-  # retrieve today's weekday (0=Monday, 6=Sunday)
-
+  # # retrieve today's weekday (0=Monday, 6=Sunday)
+  # print("Todays weekday # is: ", today.weekday())
+  # days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
+  # print("Which is a: ", days[today.weekday()])
   
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
-
+  today = datetime.now()
+  print("The current date and time is ", today)
 
   # Get the current time
-
+  t = datetime.time(datetime.now())
+  print(t)
 
   
 if __name__ == "__main__":
--- Exercise Files/Ch3/formatting_start.py
+++ Exercise Files/Ch3/formatting_start.py
@@ -7,19 +7,27 @@ from datetime import datetime
 def main():
   # Times and dates can be formatted using a set of predefined string
   # control codes 
+  now = datetime.now()
 
   #### Date Formatting ####
   
   # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
-
+  # print(now.strftime("The current year is: %Y"))
+  # print(now.strftime("%a, %d %B, %y"))
 
   # %c - locale's date and time, %x - locale's date, %X - locale's time
+  # print(now.strftime("Locale date and time: %c"))
+  # print(now.strftime("Locale date: %x"))
+  # print(now.strftime("Locale time: %X"))
 
 
   #### Time Formatting ####
   
   # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
+  print(now.strftime("Current time: %I:%M:%S %p"))
+  print(now.strftime("24-hour time: %H:%M"))
 
+  
 
 
 if __name__ == "__main__":
